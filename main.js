f14d167f9ad35fe3ca8db13ab47d38848b80b26a
ryutaProgram <60117691+ryutaProgram@users.noreply.github.com>
2020-01-28 11:29:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
   btn.addEventListener('click', function() {
     for (var i = 1; i < 10000; i++) {
-      var n = Math.floor((Math.random() * ) + 1);
+      var n = Math.floor((Math.random() * 319) + 1);
       if (n === 1) {
         this.textContent = '当たり';
         break;
